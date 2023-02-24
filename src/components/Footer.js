b5aa3915206f5ac25ca0a8e53f1f5e961e30ec67
renzoxst <lorenzo@asianmediarights.com>
2023-02-24 17:12:50 -0500

    Update node modules

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ function Footer () {
     return (
         <footer class="center">
                 <div class="row">
-                    <a href="https://www.linkedin.com/in/lorenzo-street-simmons-65098a82/">LinkedIn</a> |
-                    <a href="https://github.com/Lorenzoxst">GitHub</a> |
+                    <a href="https://www.linkedin.com/in/lorenzo-street-simmons-65098a82/">LinkedIn</a> | {" "}
+                    <a href="https://github.com/Lorenzoxst">GitHub</a> | {" "}
                     <a href={Resume} download="Lorenzo Street Resume">Resume</a>
                 </div>
         </footer>
